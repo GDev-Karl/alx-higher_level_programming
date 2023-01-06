4fa685377fd014b3753431954b47d463dcd1c23c
Karl <tekamkarl@gmail.com>
2023-01-06 03:00:51 +0100

    count argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#g!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    cpt = len(sys.argv) - 1
+    if cpt == 0:
+        print("0 arguments.")
+    elif cpt == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(cpt):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
