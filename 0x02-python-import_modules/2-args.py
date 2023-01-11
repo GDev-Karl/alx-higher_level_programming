1038d11070a2b2c21fdd1c1f6e1c7f24017c2cdd
Karl <tekamkarl@gmail.com>
2023-01-11 03:28:34 +0100

    the result of the addition of all arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import sys
-count = len(sys.argv) - 1
-
-if count == 0:
-    print("{} arguments.".format(count))
+    from sys import argv
+argc = len(argv)
+if argc < 2:
+    print("{} arguments.".format(argc - 1))
 else:
-    if count == 1:
-        print("{} argument:".format(count))
+    if argc == 2:
+        print("{} argument:".format(argc - 1))
     else:
-        print("{} arguments:".format(count))
-    for i in range(1, count + 1):
-        print("{}: {}".format(i , sys.argv[i]))
+        print("{} arguments:".format(argc - 1))
+    for n in range(1, argc):
+        print("{}: {}".format(n, argv[n]))
             
