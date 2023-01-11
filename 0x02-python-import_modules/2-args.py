ab8260d93ed955295575ca0698fb22abfdc70b0a
Karl <tekamkarl@gmail.com>
2023-01-11 03:03:49 +0100

    print("{} arguments.".format(count))

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,10 @@ if __name__ == "__main__":
 
     count = len(sys.argv) - 1
 
-    if count < 2:
+    if count == 0:
         print("{} arguments.".format(count))
+    else if count == 1:
+        print("{} argument.".format(count))
     else:
         print("{} arguments:".format(count))
     for i in range(1, count + 1):
