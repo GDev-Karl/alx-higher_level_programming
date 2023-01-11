e4a4d21316587a663e3553b2d4d460210b96e83d
Karl <tekamkarl@gmail.com>
2023-01-11 03:18:31 +0100

    the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,10 @@ count = len(sys.argv) - 1
 
 if count == 0:
     print("{} arguments.".format(count))
-else if count == 1:
+elif count == 1:
     print("{} argument.".format(count))
 else:
     print("{} arguments:".format(count))
-for i in range(1, count + 1):
-    print("{}: {}".format(i , sys.argv[i]))
+    for i in range(1, count + 1):
+        print("{}: {}".format(i , sys.argv[i]))
             
