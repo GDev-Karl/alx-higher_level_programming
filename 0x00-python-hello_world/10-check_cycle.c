114203ec374c403b66d61e87e13517ed27655082
Karl <tekamkarl@gmail.com>
2023-10-03 02:07:08 +0200

    check for cycle with Floyd's detection algorithm

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for cycle by using Floyd's detection algorithm
+ * @list: the head of the list
+ *
+ * Return: 1 if there is a cycle 0 ohterwise
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *slow_pointer = list;
+	listint_t *fast_pointer = list;
+
+	while (fast_pointer != NULL && fast_pointer->next->next != NULL)
+	{
+		slow_pointer = slow_pointer->next;
+		fast_pointer = fast_pointer->next->next;
+
+		if (slow_pointer == fast_pointer)
+			return (1);
+	}
+
+	return (0);
+}
