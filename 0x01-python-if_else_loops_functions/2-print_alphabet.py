b8f032735202720661f1142a1142b6186926224b
Karl <tekamkarl@gmail.com>
2023-10-03 23:36:09 +0200

    prints the ASCII alphabet, in lowercase,

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# store the ascii code for the first charater 'a'
+ascii_code = 97
+
+while ascii_code <= 122:
+    print(chr(ascii_code), end="")
+    ascii_code = ascii_code + 1
