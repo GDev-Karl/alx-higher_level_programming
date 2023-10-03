11b1963bc24b2c34000e82ab470ea1f04fe4f2c7
Karl <tekamkarl@gmail.com>
2023-10-03 02:20:15 +0200

    check for cwith Floyd's detection algorithm

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,10 @@ int check_cycle(listint_t *list)
 	listint_t *slow_pointer = list;
 	listint_t *fast_pointer = list;
 
-	while (fast_pointer != NULL && fast_pointer->next->next != NULL)
+	if (list == NULL)
+		return (0);
+
+	while (slow_pointer && fast_pointer && fast_pointer->next->next)
 	{
 		slow_pointer = slow_pointer->next;
 		fast_pointer = fast_pointer->next->next;
