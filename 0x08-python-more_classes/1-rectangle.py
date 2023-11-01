e2d03913e1ae4208d8471df0db5297533990a4f7
Karl <tekamkarl@gmail.com>
2023-11-01 12:03:21 +0100

    add getters and setters to Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,33 +14,41 @@ class Rectangle:
         """
         retrieves width
         """
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """ 
         set the value of width
         """
-        if type(self.width) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if self.width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
         """
         retrieves height
         """
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """
         set the value of height
         """
-        if type(self.height) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if self.height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.height = value
+        self.__height = value
+
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
