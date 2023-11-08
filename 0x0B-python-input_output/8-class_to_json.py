8cc955d3a8399c62e502a2a35ab848c693896295
Karl <tekamkarl@gmail.com>
2023-11-08 03:20:19 +0100

    dictionary description with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module on JSON """
+import json
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    for JSON serialization of an object
+    """
+    return obj.__dict__
