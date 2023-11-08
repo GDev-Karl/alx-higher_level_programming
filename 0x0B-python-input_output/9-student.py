11cb92335a4f6382e8f347c61d95fdc75314ee61
Karl <tekamkarl@gmail.com>
2023-11-08 03:20:38 +0100

    dictionary representation of the Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module on JSON """
+import json
+
+
+class Student:
+    """
+    Define a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        dictionary representation of the Student
+        """
+        return self.__dict__
