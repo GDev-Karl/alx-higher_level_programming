e5ac3e88e089e3c0ef252712c269e5e3581f5cb0
Karl <tekamkarl@gmail.com>
2023-11-09 03:33:50 +0100

    define a base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module Base Geometry"""
+
+
+class BaseGeometry:
+    """define a base geometry"""
+
+    def area(self):
+        """ area of the figure """
+        raise Exception("area() is not implemented")
