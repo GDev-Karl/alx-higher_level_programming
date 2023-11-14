974af65dbe79da4ccd82d997ed0f546743eb56ab
Karl <tekamkarl@gmail.com>
2023-11-14 02:35:17 +0100

    test the base class

---
+++ 0x0C-python-almost_a_circle/tests/0-main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" test the base class """
+from models.base import Base
+
+if __name__ == "__main__":
+
+    bs1 = Base()
+    print(bs1.id)
+
+    bs2 = Base()
+    print(bs2.id)
+
+    bs3 = Base()
+    print(bs3.id)
+
+    bs4 = Base(12)
+    print(bs4.id)
+
+    bs5 = Base()
+    print(bs5.id)
